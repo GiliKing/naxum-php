56d86e221fe36388bac6606693fbb00a635f2e66
giliking <chrisogili12@gmail>
2022-11-10 12:31:53 -0800

    first commit

--- database/connect.php
+++ database/connect.php
@@ -1,14 +1,13 @@
 <?php
 
 // local host connection variable
-$hostname = "localhost";
-$user_name = "root";
-$user_pass  = "";
-$base_name = "reg-page";
+$hostname = "us-cdbr-east-06.cleardb.net";
+$user_name = "bdea5e57a54478";
+$user_pass  = "cc1f0c28";
+$base_name = "heroku_0b7851e02efd837";
 
 
 // connect to the database 
 $conn = mysqli_connect($hostname, $user_name, $user_pass, $base_name) or die("Could Not Connect");
-
 
 ?>
